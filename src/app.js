5399be735e267f02b51c58127dcb3470e1c390f6
Shaninath Pawar <shaninathpawar.git@gmail.com>
2025-01-01 18:50:16 +0530

    - Writing auth middleware

--- src/app.js
+++ src/app.js
@@ -2,18 +2,24 @@ const express = require('express');
 
 const app = express();
 
+const { adminAuth } = require("./middlewares/auth");
+const { userAuth } = require("./middlewares/auth");
+app.use('/admin',adminAuth);
+
+app.get("/user",userAuth,(req,res)=>{
+    res.send("User data sent");
+});
+
+app.get("/admin/getAllData",(req,res)=>{
+    res.send("All data sent");
+});
+
+app.get("/admin/deleteUser",(req,res)=>{
+    res.send("Deleted a user");
+});
+
+
 app.listen(7777,()=>{
     console.log("Server is listening on port 7777!");
 });
 
-app.get('/',(req,res)=>{
-    res.send("Hello, Dashboard!");
-});
-
-app.get('/hello',(req,res)=>{
-    res.send("Hello hello hello");
-});
-
-app.get('/test',(req,res)=>{
-    res.send('Hello from /test');  
-});
